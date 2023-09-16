f338795f22b76333f2a486c7e9808bc33088cd48
bjo-droid <bjohnstad@gmail.com>
2023-09-16 23:11:29 +0200

    stuff

--- js/getPosts.js
+++ js/getPosts.js
@@ -1,5 +1,6 @@
 const wordpressUrl = "https://bjotech.no/blog/wp-json/wp/v2/posts?_embed";
 const article = document.querySelector(".articles")
+const wordpressUrlMore = "https://bjotech.no/blog/wp-json/wp/v2/posts?_embed&page=2";
 
 async function getPosts() {
     try {
@@ -22,3 +23,33 @@ async function getPosts() {
 }
 
 getPosts();
+
+// get more posts
+const loadMore = document.querySelector("#loadMoreBtn");
+loadMore.addEventListener("click", loadMorePosts);
+async function loadMorePosts() {
+  try {
+    const response = await fetch(wordpressUrlMore);
+    const posts = await response.json();
+    console.log(posts);
+    for (let i = 0; i < posts.length; i++) {
+      article.innerHTML += `<div class="card">
+      <h4>${posts[i].title.rendered}</h3>
+      <img src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="${posts[i]._embedded["wp:featuredmedia"][0].alt_text}">
+      <p>${posts[i].excerpt.rendered}</p>
+      <button class="btn">
+      <a href="posts.html?id=${posts[i].id}">Read more</a>
+      </button>
+      </div>`;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  cards = document.querySelectorAll(".card");
+  if (cards.length >= 12) {
+    loadMore.style.display = "none";
+  }
+}
+
+
+
---
+++ js/getPostsTech.js
@@ -0,0 +1,27 @@
+const wordpressUrl = "https://bjotech.no/blog/wp-json/wp/v2/posts?_embed&categories=4";
+const article = document.querySelector(".articles")
+
+
+async function getPosts() {
+    try {
+        const response = await fetch(wordpressUrl);
+        const posts = await response.json();
+        console.log(posts);
+        for (let i = 0; i < posts.length; i++) {
+            article.innerHTML += `<div class="card">
+            <h4>${posts[i].title.rendered}</h3>
+            <img src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="${posts[i]._embedded["wp:featuredmedia"][0].alt_text}">
+            <p>${posts[i].excerpt.rendered}</p>
+            <button class="btn">
+            <a href="posts.html?id=${posts[i].id}">Read more</a>
+            </button>
+            </div>`;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getPosts();
+
+
